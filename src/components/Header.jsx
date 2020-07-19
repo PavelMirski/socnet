d9fad1275779a0564e1ce3f125df11a46369a39e
PavelMirski <pavel.mirski@tptlive.ee>
2020-07-19 23:23:16 +0300

    Add new react project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://assetsnffrgf-a.akamaihd.net/assets/a/wt/univ/wpub/wt_univ_lg.jpg'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profiles.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profiles = () => {
+    return  <div className='content'>
+        <div>
+            <img src='https://assetsnffrgf-a.akamaihd.net/assets/m/102018087/univ/art/102018087_univ_lsr_lg.jpg'></img>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profiles;
