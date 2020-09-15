f362c4f804a010a954270e6ca94cb14c462e6bf5
PavelMirski <pavel.mirski@gmail.com>
2020-09-16 00:08:01 +0300

    added route,brower-router and new components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,19 +4,19 @@ import s from './Navbar.module.css';
 const Navbar = () => {
     return <nav className={s.nav}>
         <div className={`${s.item} ${s.active}`}>
-            <a>Profile</a>
+            <a href="/profile">Profile</a>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a>Messages</a>
+            <a href="/dialogs">Messages</a>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a>News</a>
+            <a href='/news'>News</a>
         </div>
         <div className={s.item}>
-            <a>Music</a>
+            <a href='/music'>Music</a>
         </div>
         <div className={s.item}>
-            <a>Settings</a>
+            <a href='/settings'>Settings</a>
         </div>
     </nav>
 }
