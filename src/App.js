3e3de31511bd9c195f3f2e7eaec84b126e1deb14
PavelMirski <pavel.mirski@gmail.com>
2020-12-03 23:07:11 +0200

    follow_unFollow added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Users from "./components/Users/Users";
+import UsersContainer from "./components/Users/UsersContainer";
 
 
 const App = () => {
@@ -25,7 +25,7 @@ const App = () => {
                 <Route path='/profile'
                        render={() => <Profile/>}/>
                 <Route path='/users'
-                       render={() => <Users/>}/>
+                       render={() => <UsersContainer/>}/>
 
                 <Route path='/news' render={() => <News/>}/>
                 <Route path='/music' render={() => <Music/>}/>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,67 @@
 import React from "react";
+import styles from "./users.module.css";
 
 let Users = (props) => {
+
+    if (props.users.length === 0) {
+        props.setUsers([
+                {
+                    id: 1,
+                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtTcCMa7jFeCw2I1bQBwxjXv_ysp1lCyXTTw&usqp=CAU',
+                    followed: false,
+                    fullName: 'Pavel',
+                    status: 'Developer',
+                    location: {city: 'Tallinn', country: 'Estonia'}
+                },
+                {
+                    id: 2,
+                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtTcCMa7jFeCw2I1bQBwxjXv_ysp1lCyXTTw&usqp=CAU',
+                    followed: true,
+                    fullName: 'Dima',
+                    status: 'Elder',
+                    location: {city: 'Narva', country: 'Estonia'}
+                },
+                {
+                    id: 3,
+                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtTcCMa7jFeCw2I1bQBwxjXv_ysp1lCyXTTw&usqp=CAU',
+                    followed: false,
+                    fullName: 'Sasha',
+                    status: 'Pioner',
+                    location: {city: 'Tartu', country: 'Estonia'}
+                }
+
+            ]
+        )
+    }
     return <div>
-        USERS ZDESS
+        {
+            props.users.map(u => <div key={u.id}>
+            <span>
+                <div>
+                    <img src={u.photoUrl} className={styles.userPhoto}/>
+                </div>
+                <div>
+                    {u.followed
+                        ? <button onClick={() => {
+                            props.unfollow(u.id)
+                        }}>Unfollow</button>
+                        : <button onClick={() => {
+                            props.follow(u.id)
+                        }}>Follow</button>}
+                </div>
+            </span>
+                <span>
+                <span>
+                    <div>{u.fullName}</div>
+                    <div>{u.status}</div>
+                </span>
+                <span>
+                    <div>{u.location.country}</div>
+                    <div>{u.location.city}</div>
+                </span>
+             </span>
+            </div>)
+        }
     </div>
 }
 
