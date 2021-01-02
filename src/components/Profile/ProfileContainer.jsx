c5f25e91ce94c0227cb1596091783c0684a23a75
PavelMirski <pavel.mirski@gmail.com>
2021-01-02 22:00:24 +0200

    added HOC for Auth

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,6 +3,8 @@ import Profile from "./Profile";
 import {getUserProfile} from "../../redux/profileReducer";
 import {connect} from "react-redux";
 import { withRouter } from "react-router-dom";
+import {Redirect} from "react-router";
+import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 
 
 class ProfileContainer extends React.Component {
@@ -23,13 +25,15 @@ class ProfileContainer extends React.Component {
     }
 }
 
+let AuthRedirectComponent = withAuthRedirect(ProfileContainer);
+
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile
+    profile: state.profilePage.profile,
+    isAuth: state.auth.isAuth
 
 });
 
-
-let  WithUrlDataContainerComponent =  withRouter (ProfileContainer);
+let  WithUrlDataContainerComponent =  withRouter (AuthRedirectComponent);
 
 export default connect (mapStateToProps, {getUserProfile}) (WithUrlDataContainerComponent);
 
---
+++ src/hoc/withAuthRedirect.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import {Redirect} from "react-router";
+
+
+export const withAuthRedirect = (Component) => {
+    class RedirectComponent extends React.Component {
+        render() {
+            if (!this.props.isAuth) return <Redirect to={'/login'} />
+            return <Component {...this.props}/>
+        }
+    }
+    return RedirectComponent;
+}
